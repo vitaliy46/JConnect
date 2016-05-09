8de5535bf8c705cee83ea5ce6c65055beae91710
Vitaliy <rss9@mail.ru>
2016-05-09 21:58:16 +0300

    fix muc leave

--- app/src/main/java/com/jabber/jconnect/MucChatActivity.java
+++ app/src/main/java/com/jabber/jconnect/MucChatActivity.java
@@ -179,7 +179,7 @@ public class MucChatActivity extends AppCompatActivity implements MucChatFragmen
                         sendMessage(b);
                     }
 
-                    finish();
+                    onBackPressed();
                 }
                 break;
             default:
